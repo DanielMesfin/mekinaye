8cf06e0476cbc0908669e6d79798b15cfc97478b
daniel <danielmesfin331@gmail.com>
2024-08-18 22:52:03 -0900

    up?

--- cardealer/settings.py
+++ cardealer/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 import cloudinary
-import cloudinary_storage
+# import cloudinary_storage
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'cloudinary',
-    'cloudinary_storage',
+    # 'cloudinary_storage',
     # 'allauth.socialaccount',
 
     # # Providers
