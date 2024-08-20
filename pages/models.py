2658b1658da957816badefe231dca72dc4c514c0
daniel <danielmesfin331@gmail.com>
2024-08-19 22:31:41 -0900

    update most data

--- pages/models.py
+++ pages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -6,7 +7,8 @@ class Team(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     designation = models.CharField(max_length=255)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    # photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo = CloudinaryField("photo")
     facebook_link = models.URLField(max_length=100)
     twitter_link = models.URLField(max_length=100)
     google_plus_link = models.URLField(max_length=100)
